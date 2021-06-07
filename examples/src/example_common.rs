c847ee16816db62f8a981de008392ea5c3c6fbc0
Dario Nieuwenhuis <dirbaio@dirbaio.net>
2021-06-07 15:33:17 +0200

    No alloc

--- examples/src/example_common.rs
+++ examples/src/example_common.rs
@@ -3,22 +3,8 @@
 use nrf_softdevice_defmt_rtt as _; // global logger
 use panic_probe as _;
 
-use alloc_cortex_m::CortexMHeap;
-use core::alloc::Layout;
 use core::sync::atomic::{AtomicUsize, Ordering};
 use defmt::panic;
-
-// this is the allocator the application will use
-#[global_allocator]
-static ALLOCATOR: CortexMHeap = CortexMHeap::empty();
-
-// define what happens in an Out Of Memory (OOM) condition
-#[alloc_error_handler]
-fn alloc_error(_layout: Layout) -> ! {
-    panic!("Alloc error");
-}
-
-const HEAP_SIZE: usize = 32 * 1024; // in bytes
 
 defmt::timestamp! {"{=u64}", {
         static COUNT: AtomicUsize = AtomicUsize::new(0);
